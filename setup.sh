a22b676cbde17ab260b7b05333bf92f4a2e75d9a
Jeffrey Wilcke <stygeo@gmail.com>
2012-04-04 23:10:00 +0200

    Using current script DIR instead of PWD

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BACKUPDIR="$HOME/dotfiles.backup"
 
 echo "Backing up files to '$BACKUPDIR'."
@@ -8,11 +9,12 @@ fi
 
 mkdir -p "$BACKUPDIR"/r/
 
+
 dotfiles=(pryrc vimrc.after vimrc.before zsh_aliases zshrc zlogin gitconfig)
 for file in ${dotfiles[@]}; do
   if [ $HOME/.$file ]; then
     mv $HOME/.$file $BACKUPDIR/$file
-    ln -s $PWD/$file $HOME/.$file
+    ln -s $DIR/$file $HOME/.$file
   fi
 done
 
@@ -21,6 +23,6 @@ if [[ $HOME/.oh-my-zsh && $HOME/.oh-my-zsh/themes/stygeo.zsh-theme ]]; then
   rm $HOME/.oh-my-zsh/themes/stygeo.zsh-theme
 fi
 
-ln -s $PWD/stygeo.zsh-theme $HOME/.oh-my-zsh/themes/stygeo.zsh-theme
+ln -s $DIR/stygeo.zsh-theme $HOME/.oh-my-zsh/themes/stygeo.zsh-theme
 
 
